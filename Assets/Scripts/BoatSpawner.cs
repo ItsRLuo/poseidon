764753ae63b24e2cdbe71c8cd4c6434e6bbb4da2
unknown <chrishawkins@CHAWKINS-894.TheFacebook.com>
2017-01-21 00:04:29 -0800

    Random placeholder boat spawning

---
+++ Assets/Scripts/BoatSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatSpawner : MonoBehaviour
+{
+    public float BoatsPerSecond = 0.5f;
+    public Boat BoatPrefab;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float spawnProb = Time.deltaTime * BoatsPerSecond;
+        if (spawnProb > Random.value)
+        {
+            var boat = GameObject.Instantiate<Boat>(BoatPrefab);
+            boat.transform.position = transform.position;
+        }
+	}
+}
