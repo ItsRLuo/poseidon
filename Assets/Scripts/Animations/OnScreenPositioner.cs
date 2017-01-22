c6f472191b1a4d30570df787a65fce8d42ee2f91
Paul Tolstoi <paul.tolstoi@gmail.com>
2017-01-22 12:15:03 -0800

    minor change to design

---
+++ Assets/Scripts/Animations/OnScreenPositioner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnScreenPositioner : MonoBehaviour {
+
+	public Vector2 screen;
+
+	private Renderer _renderer;
+
+	// Use this for initialization
+	void Start () {
+		_renderer = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var w = Camera.main.ViewportToWorldPoint(screen);
+
+		var position = transform.position;
+		position.x = w.x + _renderer.bounds.extents.x;
+		position.y = w.y + _renderer.bounds.extents.y;
+
+		transform.position = position;
+	}
+}
