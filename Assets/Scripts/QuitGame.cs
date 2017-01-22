e7dcad6c48877a4cc35259efdeaf396cef566f53
Rui Mao <ruimao18@gmail.com>
2017-01-21 22:59:10 -0800

    implement quit button

--- Assets/Scripts/QuitGame.cs
+++ Assets/Scripts/QuitGame.cs
@@ -3,8 +3,13 @@ using System.Collections;
 
 public class QuitGame : MonoBehaviour {
 	public void Update() {
-		if (Input.GetKey("escape"))
-			Application.Quit();
+		if (Input.GetKey ("escape"))
+			Quit ();
 
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
 	}
 }
