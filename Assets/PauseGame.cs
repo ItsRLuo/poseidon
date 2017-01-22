dc07563bc4ea4bdda929ef95ec015d633ad98415
Rui Mao <ruimao18@gmail.com>
2017-01-21 17:33:30 -0800

    menu and buttons

---
+++ Assets/PauseGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+	protected bool paused;
+	// Use this for initialization
+	public bool CheckIfPaused () {
+		return paused;
+	}
+	public void OnPauseGame () {
+		paused = true;
+		Time.timeScale = 0;
+	}
+	
+	// Update is called once per frame
+	public void OnResumeGame () {
+		paused = false;
+		Time.timeScale = 1;
+		
+	}
+}
