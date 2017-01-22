39c71ab56609f7a4585026829efa60808573547d
rahul <rahul.singh2@outlook.com>
2017-01-21 22:10:00 -0800

    Adding tower health bar

--- Assets/GameplayScene.cs
+++ Assets/GameplayScene.cs
@@ -1,13 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameplayScene : MonoBehaviour
 {
     public GameObject LightningPrefab;
+    public Slider TowerHealthSlider;
+    public Transform TowerLocation;
+    public Camera Camera;
+    public int TowerHealth = 1;
 
     public void Start()
-    { }
+    {
+        #region Initialize tower health slider
+        Vector2 viewportPosition = this.Camera.WorldToScreenPoint(this.TowerLocation.position);
+
+        Debug.Log(viewportPosition);
+        this.TowerHealthSlider.value = this.TowerHealth;
+        RectTransform rt = this.TowerHealthSlider.GetComponent<RectTransform>();
+        rt.anchoredPosition3D = new Vector3(viewportPosition.x, viewportPosition.y, 0);
+        #endregion
+    }
 
     public void Update()
     {
