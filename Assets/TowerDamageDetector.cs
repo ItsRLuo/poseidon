b7fa943d4297e34b40b223e6febaa5adc7ad64a2
paul <paul@cueb.io>
2017-01-22 12:36:37 -0800

    null check fix

--- Assets/TowerDamageDetector.cs
+++ Assets/TowerDamageDetector.cs
@@ -20,7 +20,8 @@ public class TowerDamageDetector : MonoBehaviour
 
 		yield return new WaitForSeconds(1.2f);
 
-        if (bc != null && bc.Sinking) yield break;
+		if (bc == null) yield break;
+        if (bc.Sinking) yield break;
 
 		var explosion = GameObject.Instantiate<GameObject>(ExplosionPrefab);
         explosion.transform.position = bc.transform.position;
